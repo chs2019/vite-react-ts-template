7bfb72228d91dc0eeae5f54669e5b96310cee35f
Christian Schmidt <47507599+chs2019@users.noreply.github.com>
2024-08-21 17:38:30 +0200

    configure vitest, husky pre-commit and eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  }
-})
+    environment: "jsdom",
+    include: ["**/*.test.{ts,tsx}"],
+    setupFiles: ["./src/setupTests.ts"],
+    css: true,
+    reporters: ["default", "html"],
+    coverage: {
+      reportsDirectory: "html",
+      include: ["**/*.{ts,tsx}"],
+      exclude: ["src/test/**/*.{ts,tsx}"],
+      reporter: ["text", ["html", { subdir: "coverage" }]],
+      provider: "v8",
+    },
+  },
+});
