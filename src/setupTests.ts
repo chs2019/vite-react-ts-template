d5ecd197f69583818daf58a87a62ade2fb1822b5
Christian Schmidt <47507599+chs2019@users.noreply.github.com>
2024-08-21 16:33:30 +0200

    initial commit

---
+++ src/setupTests.ts
@@ -0,0 +1,11 @@
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+import '@testing-library/jest-dom/vitest'
+import { cleanup } from '@testing-library/react'
+import { afterEach } from 'vitest'
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup()
+})
